a54075e4a4048418de09d59bda5a58dbb40f5784
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-11-19 19:07:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const panel=document.querySelectorAll(".panel");
+panel.forEach((panelElement)=>{
+	panelElement.addEventListener('click',()=>{
+		removeElem();
+		panelElement.classList.add('active');
+	})
+})
+
+function removeElem(){
+	panel.forEach((e)=>e.classList.remove('active'))
+}
